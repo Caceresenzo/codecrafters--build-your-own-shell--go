a4d8f53bbe2148a0a9b32c94a092a394298a0823
Caceresenzo <caceresenzo1502@gmail.com>
2024-06-22 20:25:28 +0200

    pass 7th stage

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -56,13 +56,32 @@ func builtin_echo(arguments []string) {
 func builtin_type(arguments []string) {
 	program := arguments[1]
 
-	_, found := builtins[program]
-	if found {
+	if _, found := builtins[program]; found {
 		fmt.Fprintf(os.Stdout, "%s is a shell builtin\n", program)
 		return
 	}
 
+	if path, found := locate(program); found {
+		fmt.Fprintf(os.Stdout, "%s is %s\n", program, path)
+		return
+	}
+
 	fmt.Fprintf(os.Stdout, "%s: not found\n", program)
+}
+
+func locate(program string) (string, bool) {
+	PATH := os.Getenv("PATH")
+	directories := strings.Split(PATH, ":")
+
+	for _, directory := range directories {
+		path := fmt.Sprintf("%s/%s", directory, program)
+
+		if _, error := os.Stat(path); error == nil {
+			return path, true
+		}
+	}
+
+	return "", false
 }
 
 func main() {
