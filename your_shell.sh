0975dedfffc4a1aebaf6e64d595e134dc4bbbc9f
Caceresenzo <caceresenzo1502@gmail.com>
2025-05-17 16:22:02 +0200

    CLI tests (cli-test-1747491722801)

--- your_shell.sh
+++ your_shell.sh
@@ -6,10 +6,17 @@
 #
 # DON'T EDIT THIS!
 set -e
+set -x
 
 tmpFile=$(mktemp)
 
-( cd $(dirname "$0") &&
-	go build -o "$tmpFile" ./cmd/myshell )
+free
 
-exec "$tmpFile" "$@"
+cd /app
+go build -o "$tmpFile" ./cmd/myshell && echo "done" || echo "failed"
+
+echo "running"
+
+"$tmpFile" "$@"
+
+echo "done"
