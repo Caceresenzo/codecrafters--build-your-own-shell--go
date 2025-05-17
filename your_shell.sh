479a5e59aa1b84fc243ee353fdedfaa7066bc671
Caceresenzo <caceresenzo1502@gmail.com>
2025-05-17 16:17:36 +0200

    CLI tests (cli-test-1747491456281)

--- your_shell.sh
+++ your_shell.sh
@@ -5,11 +5,25 @@
 # CodeCrafters uses this file to test your code. Don't make any changes here!
 #
 # DON'T EDIT THIS!
-set -e
+# set -e
+set -x
 
 tmpFile=$(mktemp)
+
+echo "building"
+
+sleep 1
+echo "building2"
+
+sleep 3
+echo "building3"
+
 
 ( cd $(dirname "$0") &&
 	go build -o "$tmpFile" ./cmd/myshell )
 
-exec "$tmpFile" "$@"
+echo "running"
+
+"$tmpFile" "$@"
+
+echo "done"
