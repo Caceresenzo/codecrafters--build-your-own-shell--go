3466cef17d7ef9cb3f149e6fdc4fe06c3f121812
Caceresenzo <caceresenzo1502@gmail.com>
2025-06-14 16:17:40 +0200

    CLI tests (cli-test-1749910660334)

--- app/history.go
+++ app/history.go
@@ -1,3 +1,38 @@
 package main
 
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
 var history []string
+
+func printHistory(start int, io Io) {
+	for i, line := range history[start:] {
+		fmt.Fprintf(io.Output(), "%5d  %s\n", start+i+1, line)
+	}
+}
+
+func readHistoryFrom(path string) bool {
+	file, err := os.Open(path)
+	if err != nil {
+		return false
+	}
+	defer file.Close()
+
+	var line string
+	for {
+		_, err := fmt.Fscanf(file, "%s\n", &line)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+
+			return false
+		}
+		history = append(history, line)
+	}
+
+	return true
+}
