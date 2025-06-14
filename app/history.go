b2ca413f074dfda82a91913d0992002221a0666d
Caceresenzo <caceresenzo1502@gmail.com>
2025-06-14 16:22:12 +0200

    pass 1st history persistence

--- app/history.go
+++ app/history.go
@@ -1,3 +1,34 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
 var history []string
+
+func printHistory(start int, io Io) {
+	for i, line := range history[start:] {
+		fmt.Fprintf(io.Output(), "%5d  %s\n", start+i+1, line)
+	}
+}
+
+func readHistoryFrom(path string) bool {
+	file, err := os.Open(path)
+	if err != nil {
+		return false
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		history = append(history, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		return false
+	}
+
+	return true
+}
